956435a77256d09b15e570453a08e4001daefd6d
unknown <srinidhihm98@gmail.com>
2024-10-08 22:31:32 +0530

    React code

--- App.js
+++ App.js
@@ -1,34 +1,63 @@
-/**
- * <div id= "parent">
- * <div id ="child">
- * <h1></h1>
- * </div>
- * </div>
- * ReactElement(object) =>HTML(Browser to understands)
- */
-const parent = React.createElement(
-"div",
-{ id : "parent"} ,[React.createElement(
-    "div",
-    { id : "child"} ,
-[React.createElement("h1" ,{},"iam a h1 tag"),
-    React.createElement("h2" ,{},"iam a h1 tag")
-]
-),
-React.createElement(
-    "div",
-    { id : "child"} ,
-[React.createElement("h1" ,{},"iam a h1 tag"),
-    React.createElement("h2" ,{},"iam a h1 tag")
-]
-)]
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const Header = () =>{
+    return(
+        <div className= "header">
+            <div className="logo-container">
+                <img className="logo" src ="https://png.pngtree.com/template/20191024/ourmid/pngtree-food-delivery-logo-design-fast-delivery-service-sign-image_323015.jpg"/>
+            </div>
+            <div className = "nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+const RestaurantCard = (props) =>{
+    const {resName , cusine}= props;
+  return(
+<div className ="res-card">
+    <img className= "reslogo" src="https://i0.wp.com/foodntravel.in/wp-content/uploads/2023/02/Fresh-Catch-Seafood-Restaurant.jpg?fit=1280%2C720&ss"/>
+    <h3>{resName}</h3>
+    <h4>{cusine}</h4>
+    <h4>4.4 Stars</h4>
+    <h4>38 minutes</h4>
+</div>
+  );
+};
+
+const Body = ()=> {
+    return(
+        <div className="body">
+            <div className ="search">Search</div>
+            <div className = "res-container">
+                <RestaurantCard
+                 resName="Meghana Food" 
+                 cusine= "North Indian, Asian"/>
+                <RestaurantCard
+                 resName="KFC" 
+                 cusine= "Burger, Fast Food"/>
+
+            </div>
+        </div>
+    );
+};
+
+const AppLayout = () => {
+return (
+    <div className ="app">
+   <Header/>
+   <Body/>
+    </div>
 )
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
 
-// const heading = React.createElement(
-//     "h1" ,
-//     {id : "heading",  alt : "heaidng"},
-//      "Hi Sreenidhi"
-//     );
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 #REACT TUTORIALS 
+
+#Parcel
+- Dev
+- local server
+- HMR = Hot Module Replacement
+- File Watching Algorithem - written in C++
+- Faster Build = Caching
+- Image Optimization
+- minification 
+- Bundling
+- Compressing The File
+- Https
+- Tree Shaking - remove unused code 
+-  Different dev and Production Bundles
